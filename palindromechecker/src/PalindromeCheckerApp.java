6d36b5428b2d3985acc6e466cbe2883c70e947f9
akshita2106 <akshitashah2106@gmail.com>
2026-03-11 10:00:26 +0530

    UC12: Strategy Pattern for Palindrome Algorithms (Advanced)

--- palindromechecker/src/PalindromeCheckerApp.java
+++ palindromechecker/src/PalindromeCheckerApp.java
@@ -1,17 +1,61 @@
-class PalindromeChecker {
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
-    // Method to check palindrome
+/**
+ * INTERFACE - PalindromeStrategy
+ */
+interface PalindromeStrategy {
+    boolean checkPalindrome(String input);
+}
+
+/**
+ * CLASS - StackStrategy
+ */
+class StackStrategy implements PalindromeStrategy {
+
+    /**
+     * Implements palindrome validation using Stack.
+     * @param input input string to validate
+     * @return true if palindrome, false otherwise
+     */
     public boolean checkPalindrome(String input) {
 
-        int start = 0;
-        int end = input.length() - 1;
+        Stack<Character> stack = new Stack<>();
 
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
+        // Push characters to stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        // Compare stack with original
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
+        }
+
+        return true;
+    }
+}
+
+/**
+ * CLASS - DequeStrategy
+ */
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String input) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
         }
 
         return true;
@@ -21,21 +65,19 @@ class PalindromeChecker {
 public class PalindromeCheckerApp {
 
     /**
-     * Application entry point for UC11
+     * Application entry point
      */
     public static void main(String[] args) {
 
-        // Define input string
-        String input = "radar";
+        String input = "madam";
 
-        // Create object of PalindromeChecker
-        PalindromeChecker checker = new PalindromeChecker();
+        // Inject strategy at runtime
+        PalindromeStrategy strategy = new StackStrategy();
+        // PalindromeStrategy strategy = new DequeStrategy();
 
-        // Call method
-        boolean isPalindrome = checker.checkPalindrome(input);
+        boolean result = strategy.checkPalindrome(input);
 
-        // Print result
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
     }
 }
